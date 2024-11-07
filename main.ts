e23a6d6c475b2572229407d9d9ad4fc392028c17
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-11-06 21:55:44 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,93 +7,31 @@
 
 // variables
 let sprite: game.LedSprite = null
+let counter = 0
+let spritemove = 0
 
 // happy
 basic.showIcon(IconNames.Happy)
 
 // on button A edge moving
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    counter = 0
     sprite = game.createSprite(0, 0)
+    basic.pause(100)
 
-    // loop setup
-    let counter = 0
-
-    while (counter <= 4) {
-        if (counter == 0) {
-            // move the top now
-            while (sprite.get(LedSpriteProperty.X) < 4) {
-                sprite.change(LedSpriteProperty.X, 1)
-                basic.pause(500)
-            }
+// setting up the movement
+    while (counter < 4) {
+        spritemove = 0
+        while (spritemove < 4) {
+            sprite.move(1)
+            basic.pause(100)
+            spritemove++
         }
-        if (counter == 1) {
-            // move right column
-            while (sprite.get(LedSpriteProperty.Y) < 4) {
-                sprite.change(LedSpriteProperty.Y, 1)
-                basic.pause(500)
-            }
-        }
-        if (counter == 2) {
-            // move bottom row
-            while (sprite.get(LedSpriteProperty.X) > 0) {
-                sprite.change(LedSpriteProperty.X, -1)
-                basic.pause(500)
-            }
-        }
-        if (counter == 3) {
-            // moves left column
-            while (sprite.get(LedSpriteProperty.Y) > 0) {
-                sprite.change(LedSpriteProperty.Y, -1)
-                basic.pause(500)
-            }
-        }
-        counter += 1
+        sprite.turn(Direction.Right, 90)
+        counter++
     }
-
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
-})
-
-// on button B edge moving counter clockwise
-input.onButtonPressed(Button.B, function () {
-    sprite = game.createSprite(4, 0)
-
-    // loop setup
-    let counter = 0
-
-    while (counter >= 0) {
-        if (counter == 0) {
-            // move right column
-            while (sprite.get(LedSpriteProperty.Y) < 4) {
-                sprite.change(LedSpriteProperty.Y, 1)
-                basic.pause(500)
-            }
-        }
-        if (counter == 1) {
-            // move bottom row
-            while (sprite.get(LedSpriteProperty.X) > 0) {
-                sprite.change(LedSpriteProperty.X, -1)
-                basic.pause(500)
-            }
-        }
-        if (counter == 2) {
-            // move left column
-            while (sprite.get(LedSpriteProperty.Y) > 0) {
-                sprite.change(LedSpriteProperty.Y, -1)
-                basic.pause(500)
-            }
-        }
-        if (counter == 3) {
-            // move top row
-            while (sprite.get(LedSpriteProperty.X) < 4) {
-                sprite.change(LedSpriteProperty.X, 1)
-                basic.pause(500)
-            }
-        }
-        counter += 1
-        basic.clearScreen()
-    }
-
+    
     sprite.delete()
     basic.showIcon(IconNames.Happy)
 })
